703c973998a5e93b551487d177bdc6914294f737
Volodymyr <vlad5855051@gmail.com>
2018-07-26 14:03:22 +0300

    task revers integer

---
+++ src/com/leetcode/ReverseInteger.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+public class ReverseInteger {
+    public static void main(String[] args) {
+        Solution1 solution1 = new Solution1();
+        System.out.println(solution1.reverse(5416516));
+    }
+}
+
+class Solution1 {
+    public int reverse(int x) {
+        for (int i = 0; i < 10000; i++) {
+            i++;
+        }
+        int ost;
+        long rez = 0;
+        while (x != 0) {
+            ost = x % 10;
+            rez = rez * 10 + ost;
+            x /= 10;
+        }
+        if (rez != (int) rez) {
+            return 0;
+        }
+        return (int) rez;
+    }
+}
